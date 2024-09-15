20e1c792b7cd8eb17fd58669469cc23f1d924527
funera1 <funeral7776@gmail.com>
2024-09-15 11:58:50 +0900

    Implement open/close devices

--- net.c
+++ net.c
@@ -14,26 +14,69 @@ static struct net_device *devices;
 struct net_device *
 net_device_alloc(void)
 {
+    struct net_device *dev = memory_alloc(sizeof(struct net_device));
+    if (!dev) {
+        errorf("Failed to net_device_alloc");
+        return NULL;
+    }
 
+    return dev;
 }
 
 /* NOTE: must not be call after net_run() */
 int
 net_device_register(struct net_device *dev)
 {
+    if (dev == NULL) {
+        errorf("device is null.");
+        return -1;
+    }
     
+    // NOTE: dev->index = index++の実装がよくわからない。indexをずらしていく方法だとdevicesの全部のindexをずらさないとだめで、効率悪くないか?
+    static unsigned int index = 0;
+    dev->index = index++;
+    snprintf(dev->name, sizeof(dev->name), "net%d", dev->index);
+
+    dev->next = devices;
+    devices = dev;
+    infof("registerd, dev=%s, type=0x%04x", dev->name, dev->type);
+    return 0;
 }
 
 static int
 net_device_open(struct net_device *dev)
 {
+    if (NET_DEVICE_IS_UP(dev)) {
+        errorf("already opened, dev=%s", dev->name);
+        return -1;
+    }
     
+    if (dev->ops->open(dev) == -1) {
+        errorf("failure open, dev=%s", dev->name);
+        return -1;
+    }
+    
+    dev->flags |= NET_DEVICE_FLAG_UP;
+    infof("dev=%s, state=%s", dev->name, NET_DEVICE_STATE(dev));
+    return 0;
 }
 
 static int
 net_device_close(struct net_device *dev)
 {
+    if (!NET_DEVICE_IS_UP(dev)) {
+        errorf("already closed, dev=%s", dev->name);
+        return -1;
+    }
     
+    if (dev->ops->close(dev) == -1) {
+        errorf("failure close, dev=%s", dev->name);
+        return -1;
+    }
+    
+    dev->flags &= ~NET_DEVICE_FLAG_UP;
+    infof("dev=%s, state=%s", dev->name, NET_DEVICE_STATE(dev));
+    return 0;
 }
 
 int
